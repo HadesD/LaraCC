c6de8d2510bc4cabef12cb2edc535c1abdbd7971
Dark.Hades <dark.hades.1102@gmail.com>
2018-01-22 12:10:24 +0900

    Article return

--- src/http/controllers/api/dashboard/ArticleController.cpp
+++ src/http/controllers/api/dashboard/ArticleController.cpp
@@ -79,7 +79,57 @@ namespace app { namespace http { namespace controllers { namespace api { namespa
 
   void ArticleController::read(const int id)
   {
-    this->response().out() << "sss";
+    __APP_TRY_CATCH_BEGIN__
+    {
+      cppcms::json::value res;
+
+      try
+      {
+        app::models::Article article(id);
+
+        {
+          auto &r = res;
+
+          r["id"] = article.getId();
+          r["permalink"] = "/articles/" + article.getSlug();
+          r["title"] = article.getTitle();
+          r["type"] = article.getType();
+          r["featured"] = article.getFeatured();
+          r["content"] = article.getContent();
+
+          // TypeText
+          auto &rTypeText = r["type_text"];
+          auto typeText = article.getTypeText();
+          rTypeText["name"] = typeText.name;
+          rTypeText["icon_class_name"] = typeText.iconClassName;
+          rTypeText["featured_class_name"] = typeText.featuredClassName;
+          rTypeText["icon_class_name"] = typeText.iconClassName;
+
+          // Author
+          auto &rAuthor = r["author"];
+          rAuthor["id"] = article.getAuthorId();
+          rAuthor["url"] = "/";
+          rAuthor["name"] = article.getAuthorId();
+
+          // Tags
+          for (int t = 0; t < 0; t++)
+          {
+            auto &rTag = r["tags"][t];
+            rTag["id"] = "/";
+            rTag["url"] = "/";
+            rTag["name"] = "none";
+          }
+        }
+      }
+      catch (const app::database::ConnectorException&)
+      {
+        this->response().status(cppcms::http::response::not_found);
+        res["error"] = "error";
+      }
+
+      this->response().out() << res;
+    }
+    __APP_TRY_CATCH_END__
   }
 
   void ArticleController::create()
