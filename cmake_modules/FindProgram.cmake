a25c43d77c53bf9ffde39d7e221942627946edc8
Dark.Hades <dark.hades.1102@gmail.com>
2018-03-05 23:24:11 +0900

    Move cmake_modules

--- cmake_modules/FindProgram.cmake
+++
@@ -1,8 +0,0 @@
-find_program(PYTHON NAMES python2 python python3)
-if(NOT PYTHON)
-  message(FATAL_ERROR "Can't build without Python interpreter")
-endif()
-
-find_program(EXE_TMPL_CC
-  ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/cppcms/include/bin/cppcms_tmpl_cc
-  )
